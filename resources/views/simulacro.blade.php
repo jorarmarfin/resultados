3988f85c6f87b661453a1f2dfd04a8a6b3877092
Luis Mayta <luis.mayta@gmail.com>
2017-06-04 17:26:25 -0500

    Cambios en la forma de publicar

--- resources/views/simulacro.blade.php
+++ resources/views/simulacro.blade.php
@@ -16,58 +16,26 @@
         @if(isset($Lista))
 
         <p></p>
-            <div class="row">
-                <div class="col-md-4">
-
-                <ul class="list-group">
-                    <li class="list-group-item"><strong>Código: </strong>
-                        {{ $Lista->codigo }}
-                    </li>
-                    <li class="list-group-item"><strong>Nombres Completo: </strong>
-                        {{ $Lista->nombre_completo }}
-                    </li>
-                    <li class="list-group-item"><strong> Puntaje: </strong>
-                        {{ $Lista->ver_puntaje }}
-                    </li>
-                    <li class="list-group-item"><strong> Nota: </strong>
-                        {{ $Lista->ver_nota }}
-                    </li>
-                    <li class="list-group-item"><strong> Mérito: </strong>
-                        {{ $Lista->ver_merito }}
-                    </li>
-                </ul>
-                </div><!--span-->
-            </div><!--row-->
-            <div class="row">
-                <div class="col-md-6">
-                    <table class="table table-bordered table-hover" data-toggle="table">
-                        <thead>
-                            <tr class="info">
-                                <th> Materia </th>
-                                <th> Preguntas </th>
-                                <th> Nivel </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td> Física y Química </td>
-                                <td> {{ $Lista->ver_materia1 }} </td>
-                                <td> {{ $Lista->ver_nivel1 }} </td>
-                            </tr>
-                            <tr>
-                                <td> Matemática </td>
-                                <td> {{ $Lista->ver_materia2 }} </td>
-                                <td> {{ $Lista->ver_nivel2 }} </td>
-                            </tr>
-                            <tr>
-                                <td> CC. SS., cultura general e Inglés </td>
-                                <td> {{ $Lista->ver_materia3 }} </td>
-                                <td> {{ $Lista->ver_nivel3 }} </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div><!--span-->
-            </div><!--row-->
+            <table class="table table-bordered table-hover table-responsive" data-toggle="table" data-pagination="true">
+                <thead>
+                    <tr class="info">
+                        <th> Codigo </th>
+                        <th> Nombres  </th>
+                        <th> Puntaje </th>
+                        <th> Nota  </th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($Lista as $item)
+                    <tr >
+                        <td> {{ $item->codigo }} </td>
+                        <td> {{ $item->nombre_completo }} </td>
+                        <td> {{ $item->ver_puntaje }} </td>
+                        <td> {{ $item->ver_nota }} </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         @endif
     @endif
 @stop
