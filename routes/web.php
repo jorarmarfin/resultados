19ab0c16eab8e71889360c79b0cf4c1accba2b24
Luis Mayta <luis.mayta@gmail.com>
2017-06-04 17:10:51 -0500

    correccion en la ruta de resultado

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,12 @@ Route::get('activando/{id}','EstadoResultadosController@activar')->name('resulta
 Route::get('resultados-cepre/{id?}','ResultadosController@cepre')->name('resultados.cepre');
 Route::post('resultados-cepre','ResultadosController@storecepre')->name('resultados.cepre.store');
 
-Route::get('resultados-admision','ResultadosController@admision')->name('resultados.admision');
+Route::get('resultados-admision/{id?}','ResultadosController@admision')->name('resultados.admision');
 Route::post('resultados-admision','ResultadosController@storeadmision')->name('resultados.admision.store');
 
-Route::get('resultados-simulacro','ResultadosController@simulacro')->name('resultados.simulacro');
+Route::get('resultados-simulacro/{id?}','ResultadosController@simulacro')->name('resultados.simulacro');
 Route::post('resultados-simulacro','ResultadosController@storesimulacro')->name('resultados.simulacro.store');
 
-Route::get('resultados-escolar','ResultadosController@escolar')->name('resultados.escolar');
+Route::get('resultados-escolar/{id?}','ResultadosController@escolar')->name('resultados.escolar');
 Route::post('resultados-escolar','ResultadosController@storeescolar')->name('resultados.escolar.store');
 
